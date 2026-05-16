994e1e3db0a6a0f4c438728051dd398d8e2919d9
AkideweKuseh <98606196+AkideweKuseh@users.noreply.github.com>
2026-05-16 15:17:26 +0000

    chore(lint): add ESLint + Prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    'no-console': 'error',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '*.cjs', '*.mjs'],
+};
